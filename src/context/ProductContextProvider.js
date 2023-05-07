8d00b777ec3aa919d6e0e231609b7f5c192f5199
Masoud Ramezani <masoudhra@gmail.com>
2023-05-07 14:07:27 +1200

    start shopping cart project with react jas and make a context and get data products from fakestoreapi.com with axios with async await

---
+++ src/context/ProductContextProvider.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, createContext } from 'react';
+
+//API
+import { getProducts } from '../services/api';
+
+const ProductContext = createContext();
+
+const ProductContextProvider = (props) => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setProducts(await getProducts());
+        }
+        
+        fetchAPI();
+
+    }, []);
+
+
+    return (
+        <ProductContext.Provider value={products}>
+
+            {props.children}
+        </ProductContext.Provider>
+    );
+};
+
+export default ProductContextProvider;
