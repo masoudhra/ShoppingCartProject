6978df6e41aef69e2eac8d57c1543e5c9de12b15
Masoud Ramezani <masoudhra@gmail.com>
2023-05-11 13:32:32 +1200

    add navbar and shopCart components

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -8,7 +8,7 @@ import { shorten, isInCart, quantityCount } from '../../helper/functions';
 import { CartContext } from '../../context/CartContextProvider';
 
 // Icons
-import trashIcon from "../../assets/icon/trash.svg";
+import trashIcon from "../../assets/icons/trash.svg";
 
 const Product = ({productData}) => {
 
