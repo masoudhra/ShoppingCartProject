84f689d31c24c03b4876d1bdf77cb00445db0b4b
Masoud Ramezani <masoudhra@gmail.com>
2023-05-15 14:43:28 +1200

    add cart component and add stayle in css in my project

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -7,16 +7,19 @@ import { CartContext } from '../../context/CartContextProvider';
 // Icons
 import shopIcon from "../../assets/icons/shop.svg";
 
+// Style
+import styles from "./Navbar.module.css";
+
 const Navbar = () => {
 
     const {state} = useContext(CartContext);
 
     return (
-        <div>
-            <div>
-                <Link to="/products">Products</Link>
-                <div>
-                    <Link to="/Cart"><img src={shopIcon} alt="shop" /></Link>
+        <div className={styles.mainContainer}>
+            <div className={styles.container}>
+                <Link className={styles.productLink} to="/products">Products</Link>
+                <div className={styles.iconContainer}>
+                    <Link to="/cart"><img src={shopIcon} /></Link>
                     <span>{state.itemsCounter}</span>
                 </div>
             </div>
